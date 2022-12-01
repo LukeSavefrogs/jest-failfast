ecbfc506a395a4457979528592d0921ad0f7ff0b
Luca Salvarani <lucasalvarani99@gmail.com>
2022-12-01 21:15:18 +0100

    conf(jest): Added useful info for `testRunner`

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ import type {Config} from '@jest/types';
 const config: Config.InitialOptions = {
 	verbose: true,
 	noStackTrace: true,
+	
+	// Prior to Jest v27 this was needed in order to make `jest-circus` the test runner.
+	// In Jest >= v27, the `jest-circus` was set to default.
 	testRunner: "jest-circus/runner",
 };
 
