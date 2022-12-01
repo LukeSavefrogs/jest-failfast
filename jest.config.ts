b9bd7720fa8c61e4ef556c273a9a7d4ab44fd611
Luca Salvarani <lucasalvarani99@gmail.com>
2022-12-01 20:57:49 +0100

    conf(jest): Removed unused parameters

--- jest.config.ts
+++ jest.config.ts
@@ -5,14 +5,6 @@ const config: Config.InitialOptions = {
 	verbose: true,
 	noStackTrace: true,
 	testRunner: "jest-circus/runner",
-	testEnvironment: "<rootDir>/src/jest-environment.js",
-	// testEnvironmentOptions: {
-	// 	"enableFailFast": true
-	// },
-	transform: {
-		"\\.[jt]sx?$": "babel-jest",
-	},
-	reporters: [ "default", "summary" ],
 };
 
 module.exports = config;
